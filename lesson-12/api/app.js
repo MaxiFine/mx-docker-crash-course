cd102829c720f3016c32d2b850263b4729d6a675
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-03-05 16:06:08 +0000

    switching from react default port

--- lesson-12/api/app.js
+++ lesson-12/api/app.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
   ])
 })
 
-app.listen(4000, () => {
-  console.log('listening for requests on port 4000')
+app.listen(4500, () => {
+  console.log('listening for requests on port 4500')
 })
 
